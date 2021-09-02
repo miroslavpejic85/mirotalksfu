1e845cf87f057c61c98326a7001e9ab719387770
Miroslav Pejic <miroslav.pejic@cloud.timenet.it>
2021-09-02 16:16:31 +0200

    [mirotalksfu] - update config

--- src/config.template.js
+++ src/config.template.js
@@ -61,6 +61,37 @@ module.exports = {
                         'x-google-start-bitrate': 1000,
                     },
                 },
+                {
+                    kind: 'video',
+                    mimeType: 'video/VP9',
+                    clockRate: 90000,
+                    parameters: {
+                        'profile-id': 2,
+                        'x-google-start-bitrate': 1000,
+                    },
+                },
+                {
+                    kind: 'video',
+                    mimeType: 'video/h264',
+                    clockRate: 90000,
+                    parameters: {
+                        'packetization-mode': 1,
+                        'profile-level-id': '4d0032',
+                        'level-asymmetry-allowed': 1,
+                        'x-google-start-bitrate': 1000,
+                    },
+                },
+                {
+                    kind: 'video',
+                    mimeType: 'video/h264',
+                    clockRate: 90000,
+                    parameters: {
+                        'packetization-mode': 1,
+                        'profile-level-id': '42e01f',
+                        'level-asymmetry-allowed': 1,
+                        'x-google-start-bitrate': 1000,
+                    },
+                },
             ],
         },
         // WebRtcTransport settings
