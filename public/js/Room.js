5894bd8871bc03bf8fe9490fb97267611369473d
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2021-11-07 19:59:54 +0100

    [mirotalksfu] - added some autocompile emoji

--- public/js/Room.js
+++ public/js/Room.js
@@ -761,6 +761,26 @@ function handleInputs() {
             chatSendButton.click();
         }
     };
+    chatMessage.oninput = function () {
+        let chatInputEmoji = {
+            '<3': '\u2764\uFE0F',
+            '</3': '\uD83D\uDC94',
+            ':D': '\uD83D\uDE00',
+            ':)': '\uD83D\uDE03',
+            ';)': '\uD83D\uDE09',
+            ':(': '\uD83D\uDE12',
+            ':p': '\uD83D\uDE1B',
+            ';p': '\uD83D\uDE1C',
+            ":'(": '\uD83D\uDE22',
+            ':+1:': '\uD83D\uDC4D',
+        };
+
+        for (let i in chatInputEmoji) {
+            let regex = new RegExp(i.replace(/([()[{*+.$^\\|?])/g, '\\$1'), 'gim');
+            this.value = this.value.replace(regex, chatInputEmoji[i]);
+        }
+    };
+
     rc.getId('chatEmoji').addEventListener('emoji-click', (e) => {
         chatMessage.value += e.detail.emoji.unicode;
         rc.toggleChatEmoji();
