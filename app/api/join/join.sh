e942957032a1ee216c3f359f5a85ee950cfac832
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2022-05-21 08:50:15 +0200

    [mirotalksfu] - fix typo

--- app/api/join/join.sh
+++ app/api/join/join.sh
@@ -6,5 +6,5 @@ MIROTALK_URL="https://sfu.mirotalk.org/api/v1/join"
 curl $MIROTALK_URL \
     --header "authorization: $API_KEY" \
     --header "Content-Type: application/json" \
-    --data '{"room":"test","name":"mirotalksfu","audio":"1","video":"1","screen":"1""notify":"1"}' \
+    --data '{"room":"test","name":"mirotalksfu","audio":"1","video":"1","screen":"1","notify":"1"}' \
     --request POST
