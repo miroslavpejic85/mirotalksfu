a00bab09127e89f4d9a49f4b95c48acc769d065c
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2022-08-23 10:36:06 +0200

    [mirotalksfu] - fix participants search

--- public/js/RoomClient.js
+++ public/js/RoomClient.js
@@ -3202,7 +3202,7 @@ class RoomClient {
         table = this.getId('myTable');
         tr = table.getElementsByTagName('tr');
         for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName('td')[0];
+            td = tr[i].getElementsByTagName('td')[1];
             if (td) {
                 txtValue = td.textContent || td.innerText;
                 if (txtValue.toUpperCase().indexOf(filter) > -1) {
