fb597084f63869d8bdb6a849765099022238abb6
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-02-20 11:08:56 +0100

    [mirotalksfu] - fix typo

--- app/src/XSS.js
+++ app/src/XSS.js
@@ -5,9 +5,12 @@ const Logger = require('./Logger');
 const log = new Logger('Xss');
 
 const checkXSS = (dataObject) => {
-    const data = xss(JSON.stringify(dataObject));
-    log.debug('Sanitization done');
-    return JSON.parse(data);
+    if (typeof dataObject === 'object' && dataObject !== null) {
+        const data = xss(JSON.stringify(dataObject));
+        log.debug('Sanitization done');
+        return JSON.parse(data);
+    }
+    return dataObject;
 };
 
 module.exports = checkXSS;
