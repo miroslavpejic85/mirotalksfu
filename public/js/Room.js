4aace0e424abc8c5de02f626280cf23c396821b4
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-05-04 14:33:08 +0200

    [mirotalsfu] - add font-family...

--- public/js/Room.js
+++ public/js/Room.js
@@ -633,7 +633,7 @@ async function shareRoom(useNavigator = false) {
             </div>
             <br/>
             <p style="background:transparent; color:rgb(8, 189, 89);">Join from your mobile device</p>
-            <p style="background:transparent; color:white;">No need for apps, simply capture the QR code with your mobile camera Or Invite someone else to join by sending them the following URL</p>
+            <p style="background:transparent; color:white; font-family: Arial, Helvetica, sans-serif;">No need for apps, simply capture the QR code with your mobile camera Or Invite someone else to join by sending them the following URL</p>
             <p style="background:transparent; color:rgb(8, 189, 89);">${RoomURL}</p>`,
             showDenyButton: true,
             showCancelButton: true,
