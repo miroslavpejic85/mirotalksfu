b09dcff2784c8692c3237292b03adca6c3334a16
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-07-26 15:15:01 +0200

    [mirotalksfu] - update dep, API snippets

--- app/api/join/join.js
+++ app/api/join/join.js
@@ -1,32 +1,39 @@
 'use strict';
 
-const fetch = require('node-fetch');
+async function getJoin() {
+    try {
+        // Use dynamic import with await
+        const { default: fetch } = await import('node-fetch');
 
-const API_KEY = 'mirotalksfu_default_secret';
-const MIROTALK_URL = 'https://sfu.mirotalk.com/api/v1/join';
-// const MIROTALK_URL = 'http://localhost:3010/api/v1/join';
+        const API_KEY = 'mirotalksfu_default_secret';
+        const MIROTALK_URL = 'https://sfu.mirotalk.com/api/v1/join';
+        // const MIROTALK_URL = 'http://localhost:3010/api/v1/join';
 
-function getResponse() {
-    return fetch(MIROTALK_URL, {
-        method: 'POST',
-        headers: {
-            authorization: API_KEY,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            room: 'test',
-            password: false,
-            name: 'mirotalksfu',
-            audio: true,
-            video: true,
-            screen: true,
-            notify: true,
-        }),
-    });
+        const response = await fetch(MIROTALK_URL, {
+            method: 'POST',
+            headers: {
+                authorization: API_KEY,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                room: 'test',
+                password: false,
+                name: 'mirotalksfu',
+                audio: true,
+                video: true,
+                screen: true,
+                notify: true,
+            }),
+        });
+        const data = await response.json();
+        if (data.error) {
+            console.log('Error:', data.error);
+        } else {
+            console.log('join:', data.join);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
-getResponse().then(async (res) => {
-    console.log('Status code:', res.status);
-    const data = await res.json();
-    console.log('join:', data.join);
-});
+getJoin();
