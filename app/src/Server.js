6b08df99bc9e533e6062e4f4951e618452865233
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-09-05 08:43:51 +0200

    [mirotalksfu] - #124 keep room name through authentication

--- app/src/Server.js
+++ app/src/Server.js
@@ -241,6 +241,14 @@ function startServer() {
     app.get(['/logged'], (req, res) => {
         const ip = getIP(req);
         if (allowedIP(ip)) {
+            if (Object.keys(req.query).length > 0) {
+                log.debug('Logged: Direct Join', req.query);
+                // http://localhost:3010/?room=test or http://localhost:3010/join?room=test
+                const { room } = checkXSS(req.query);
+                if (room) {
+                    return res.sendFile(views.room);
+                }
+            }
             res.sendFile(views.landing);
         } else {
             hostCfg.authenticated = false;
