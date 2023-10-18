d2382681465c066b03efe1ad6c2ef99ed7b15be6
abhijitCogoport <abhijit.gayen@cogoport.com>
2023-10-18 17:41:38 +0530

    add

--- app/db/helper.js
+++ app/db/helper.js
@@ -1,32 +1,32 @@
-let  { FieldValue } = require('firebase-admin/firestore');
+let { FieldValue } = require('firebase-admin/firestore');
 
 
-function getMeetingRoom({firestoreDB, room_id = ''}){
+function getMeetingRoom({ firestoreDB, room_id = '' }) {
     return firestoreDB.collection("video_conference").doc(room_id).get()
 }
 
-function updateMeetingRoom({firestoreDB, room_id, data_to_update}){
-    if (room_id  && data_to_update){
+function updateMeetingRoom({ firestoreDB, room_id, data_to_update }) {
+    if (room_id && data_to_update) {
         let video_conference_ref = firestoreDB.collection("video_conference").doc(room_id);
         video_conference_ref.set(data_to_update, { merge: true })
     }
 }
 
-function updateUserCount({firestoreDB, user_list = [], room_id}){
-    if (room_id && user_list){
+function updateUserCount({ firestoreDB, user_list = [], room_id }) {
+    if (room_id && user_list) {
         let video_conference_ref = firestoreDB.collection("video_conference").doc(room_id);
         let now_time = new Date().getTime() / 1000
-    
+
         let users_in_meeting = {}
-    
-        for (user of user_list){
-            if (users_in_meeting[user.user_id]){
-                users_in_meeting[user.user_id] += 1 
-            }else{
+
+        for (user of user_list) {
+            if (users_in_meeting[user.user_id]) {
+                users_in_meeting[user.user_id] += 1
+            } else {
                 users_in_meeting[user.user_id] = 1
             }
         }
-    
+
         video_conference_ref.update({
             updated_at: now_time,
             users_in_meeting: users_in_meeting,
@@ -35,8 +35,8 @@ function updateUserCount({firestoreDB, user_list = [], room_id}){
     }
 }
 
-function joinMeetingUpdate({firestoreDB, room_id, user_id, user_name}){
-    if (user_id && room_id){
+function joinMeetingUpdate({ firestoreDB, room_id, user_id, user_name }) {
+    if (user_id && room_id) {
         let video_conference_user_ref = firestoreDB.collection("video_conference").doc(room_id).collection('user').doc(user_id)
         let now_time = new Date().getTime() / 1000
 
@@ -44,7 +44,7 @@ function joinMeetingUpdate({firestoreDB, room_id, user_id, user_name}){
             user_name: user_name || '',
             join_at: now_time,
             updated_at: now_time,
-            activity_time_lines : FieldValue.arrayUnion({
+            activity_time_lines: FieldValue.arrayUnion({
                 action: 'join_meeting',
                 join_at: now_time,
             })
@@ -52,20 +52,32 @@ function joinMeetingUpdate({firestoreDB, room_id, user_id, user_name}){
     }
 }
 
-function leaveMeetingUpdate({firestoreDB, room_id, user_id, user_name}){
-    if(user_id && room_id){
+function leaveMeetingUpdate({ firestoreDB, room_id, user_id, user_name }) {
+    if (user_id && room_id) {
         let video_conference_user_ref = firestoreDB.collection("video_conference").doc(room_id).collection('user').doc(user_id)
         let now_time = new Date().getTime() / 1000
 
         video_conference_user_ref.set({
-            user_name: user_name| '',
+            user_name: user_name | '',
             leave_at: now_time,
             updated_at: now_time,
-            activity_time_lines : FieldValue.arrayUnion({
+            activity_time_lines: FieldValue.arrayUnion({
                 action: 'leave_meeting',
                 join_at: now_time,
             })
         }, { merge: true })
+
+        firestoreDB.collection("user").doc(user_id).get().then((user_ref_doc) => {
+            if (user_ref_doc) {
+                user_room_data = user_ref_doc.data()
+                if (user_room_data.meeting_id === room_id) {
+                    firestoreDB.collection("user").doc(user_id).update({
+                        video_call_status: 'completed',
+                        updated_at: now_time
+                    })
+                }
+            }
+        })
     }
 }
 
