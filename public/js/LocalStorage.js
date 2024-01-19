e93211b1ecb82f5c71ac0ac4269f017f1331de0a
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-01-19 09:03:12 +0100

    [mirotalksfu] - set default screen 5fps

--- public/js/LocalStorage.js
+++ public/js/LocalStorage.js
@@ -37,7 +37,7 @@ class LocalStorage {
             mic_latency: 50, // ms
             mic_volume: 100, // %
             video_fps: 0, // default 1280x768 30fps
-            screen_fps: 0, // max 30fps
+            screen_fps: 3, // default 5fps
             broadcasting: false, // default false (one to many a/v streaming)
             lobby: false, // default false
             pitch_bar: true, // volume indicator
