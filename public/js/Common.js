5a233a004a65749157a1849209ee625eb942be61
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-01-19 19:54:51 +0100

    [mirotalksfu] - update common

--- public/js/Common.js
+++ public/js/Common.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const features = document.getElementById('features');
+const teams = document.getElementById('teams');
+const tryEasier = document.getElementById('tryEasier');
 const poweredBy = document.getElementById('poweredBy');
 const sponsors = document.getElementById('sponsors');
 const advertisers = document.getElementById('advertisers');
@@ -8,6 +11,9 @@ const footer = document.getElementById('footer');
 
 const config = {
     html: {
+        features: true,
+        teams: true, // please keep me always true ;)
+        tryEasier: true,
         poweredBy: true,
         sponsors: true,
         advertisers: true,
@@ -16,6 +22,9 @@ const config = {
     //...
 };
 
+!config.html.features && elementDisplay(features, false);
+!config.html.teams && elementDisplay(teams, false);
+!config.html.tryEasier && elementDisplay(tryEasier, false);
 !config.html.poweredBy && elementDisplay(poweredBy, false);
 !config.html.sponsors && elementDisplay(sponsors, false);
 !config.html.advertisers && elementDisplay(advertisers, false);
