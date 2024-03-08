07d829bb431c3e9fa66679045b333a4176027979
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-03-08 11:35:53 +0100

    [mirotalksfu] - update mediasoup to latest

--- public/js/RoomClient.js
+++ public/js/RoomClient.js
@@ -9,7 +9,7 @@
  * @license For commercial or closed source, contact us at license.mirotalk@gmail.com or purchase directly via CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-sfu-webrtc-realtime-video-conferences/40769970
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.3.86
+ * @version 1.3.87
  *
  */
 
