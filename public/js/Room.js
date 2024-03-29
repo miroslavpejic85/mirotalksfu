69385c21785e91e9bdba2e9c37f96a801d97287b
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-03-29 16:58:10 +0100

    [mirotalksfu] -  update dep

--- public/js/Room.js
+++ public/js/Room.js
@@ -11,7 +11,7 @@ if (location.href.substr(0, 5) !== 'https') location.href = 'https' + location.h
  * @license For commercial or closed source, contact us at license.mirotalk@gmail.com or purchase directly via CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-sfu-webrtc-realtime-video-conferences/40769970
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.4.1
+ * @version 1.4.11
  *
  */
 
