fedc8aa43090184a017ecaf87710807717b80e1b
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-08-09 17:36:03 +0200

    [mirotalksfu] - fix typo

--- public/js/Room.js
+++ public/js/Room.js
@@ -377,7 +377,7 @@ function refreshMainButtonsToolTipPlacement() {
         setTippy('pollButton', 'Toggle the poll', placement);
         setTippy('transcriptionButton', 'Toggle transcription', placement);
         setTippy('whiteboardButton', 'Toggle the whiteboard', placement);
-        setTippy('snapshotRoomButton', 'Snapshot Screen, Window, or Tab', placement);
+        setTippy('snapshotRoomButton', 'Snapshot screen, window, or tab', placement);
         setTippy('settingsButton', 'Toggle the settings', placement);
         setTippy('aboutButton', 'About this project', placement);
 
