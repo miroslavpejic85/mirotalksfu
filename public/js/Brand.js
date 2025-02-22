2755210972258d42e3c68bbcf4d267caf6461d21
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-02-22 21:46:12 +0100

    [mirotalksfu] - Improve settings UI on small desktop screen

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -64,7 +64,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v1.7.55</strong>',
+        title: '<strong>WebRTC SFU v1.7.56</strong>',
         html: `
             <button 
                 id="support-button" 
