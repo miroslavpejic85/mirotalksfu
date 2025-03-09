b8570c593e4dd987b81e6a4a32d695592b62dc42
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-03-09 10:44:32 +0100

    [mirotalksfu] - fix decodeURIComponent

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -64,7 +64,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v1.7.73</strong>',
+        title: '<strong>WebRTC SFU v1.7.74</strong>',
         html: `
             <button 
                 id="support-button" 
