bc8b7e94bcafa978aa61637116a93510def98631
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-03-24 20:51:30 +0100

    [mirotalksfu] - fix typo

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -116,7 +116,6 @@ module.exports = {
         cpu: {
             cores: os.cpus().length,
             model: os.cpus()[0].model,
-            speed: os.cpus()[0].speed + ' MHz',
         },
         memory: {
             total: (os.totalmem() / 1024 / 1024 / 1024).toFixed(2) + ' GB',
