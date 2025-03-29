2d1ca5f84b4b08e114b7ce55515e1208bff293eb
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-03-29 16:50:01 +0100

    [mirotalksfu] - add powered by mediasoup

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -64,7 +64,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v1.8.00</strong>',
+        title: '<strong>WebRTC SFU v1.8.01</strong>',
         html: `
             <button 
                 id="support-button" 
