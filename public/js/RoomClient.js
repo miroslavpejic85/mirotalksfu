33f68be2fa68cb4d58ae9d1ea0a585da4e441b01
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-08-16 14:02:50 +0200

    [mirotalksfu] - fix getAudioConstraints, update dep

--- public/js/RoomClient.js
+++ public/js/RoomClient.js
@@ -9,7 +9,7 @@
  * @license For commercial or closed source, contact us at license.mirotalk@gmail.com or purchase directly via CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-sfu-webrtc-realtime-video-conferences/40769970
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.9.37
+ * @version 1.9.38
  *
  */
 
@@ -2194,8 +2194,7 @@ class RoomClient {
     // ####################################################
 
     getAudioConstraints(deviceId) {
-        let audioConstraints = {};
-        deviceId ? (audioConstraints.deviceId = deviceId) : (audioConstraints = true);
+        const audioConstraints = deviceId ? { deviceId: { exact: deviceId } } : true;
         return {
             audio: audioConstraints,
             video: false,
