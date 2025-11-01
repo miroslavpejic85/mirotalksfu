cc3fe7f993776d45a7a12395ee5c8fe6a61f0b7f
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-11-01 20:10:45 +0100

    [mirotalksfu] - improve audioConstraints

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -78,7 +78,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v1.9.95</strong>',
+        title: '<strong>WebRTC SFU v1.9.96</strong>',
         html: `
             <button 
                 id="support-button" 
