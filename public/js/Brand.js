38250cc70b492c7873b5ab73012f680a37f00504
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-11-11 15:09:05 +0100

    [mirotalksfu] - improve landing

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -79,7 +79,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.0.05</strong>',
+        title: '<strong>WebRTC SFU v2.0.06</strong>',
         html: `
             <button 
                 id="support-button" 
