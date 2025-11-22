2d0efca5dc95a7113077a3223db4ffa9aac11bec
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-11-22 04:20:12 +0100

    [mirotalksfu] - add option widget draggable

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -79,7 +79,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.0.27</strong>',
+        title: '<strong>WebRTC SFU v2.0.28</strong>',
         html: `
             <button 
                 id="support-button" 
