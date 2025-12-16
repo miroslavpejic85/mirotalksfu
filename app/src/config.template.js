3487bfe0833d94e169a64597650190292c0e8a6c
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-12-16 10:33:45 +0100

    [mirotalksfu] - improve quick devices access UI

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk SFU v2.0.60 - Configuration File
+ * MiroTalk SFU v2.0.61 - Configuration File
  * ==============================================
  *
  * This file contains all configurable settings for the MiroTalk SFU application.
--- public/js/Brand.js
+++ public/js/Brand.js
@@ -80,7 +80,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.0.60</strong>',
+        title: '<strong>WebRTC SFU v2.0.61</strong>',
         html: `
             <button 
                 id="support-button" 
