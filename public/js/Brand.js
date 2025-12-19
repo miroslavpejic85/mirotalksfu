d700840771196dc1b94c7320a9bb70d3f62f6f5d
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-12-19 23:14:04 +0100

    [mirotalksfu] - improve wb shortcuts UI popup

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -80,7 +80,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.0.71</strong>',
+        title: '<strong>WebRTC SFU v2.0.72</strong>',
         html: `
             <button 
                 id="support-button" 
