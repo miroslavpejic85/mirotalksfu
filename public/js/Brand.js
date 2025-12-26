1c9fe489affde54aead43343f26451f7deeb8283
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-12-26 21:33:39 +0100

    [mirotalksfu] - improve whiteboard

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -80,7 +80,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.0.82</strong>',
+        title: '<strong>WebRTC SFU v2.0.83</strong>',
         html: `
             <button 
                 id="support-button" 
