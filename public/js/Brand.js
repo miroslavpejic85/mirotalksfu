cfd29aab41208784c947623d68e3286ff476ee54
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-12-26 18:53:20 +0100

    [mirotalksfu] - improve settings UI

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -80,7 +80,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.0.81</strong>',
+        title: '<strong>WebRTC SFU v2.0.82</strong>',
         html: `
             <button 
                 id="support-button" 
