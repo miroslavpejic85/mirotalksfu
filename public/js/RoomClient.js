c65ea97a429c3e5d10167b7d080ecc35913ff4b0
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-01-15 13:52:16 +0100

    [mirotalksfu] - #239 mute if no audio, update dep

--- public/js/RoomClient.js
+++ public/js/RoomClient.js
@@ -9,7 +9,7 @@
  * @license For commercial or closed source, contact us at license.mirotalk@gmail.com or purchase directly via CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-sfu-webrtc-realtime-video-conferences/40769970
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 2.0.88
+ * @version 2.0.89
  *
  */
 
@@ -3318,7 +3318,12 @@ class RoomClient {
                 elem.setAttribute('id', id);
                 elem.setAttribute('volumeBar', remotePeerId + '___pVolume');
                 elem.autoplay = true;
-                elem.audio = 1.0;
+                elem.volume = 1.0;
+
+                if (!this.hasAudioTrack(stream)) {
+                    elem.muted = true;
+                }
+
                 this.remoteAudioEl.appendChild(elem);
 
                 await this.attachMediaStream(elem, stream, type, 'Consumer');
