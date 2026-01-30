6cb877d70a5a9f2ca4d3314df6b56d0c48b87879
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-01-30 02:37:52 +0100

    [mirotalksfu] - ops

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk SFU v2.0.96 - Configuration File
+ * MiroTalk SFU v2.0.97 - Configuration File
  * ==============================================
  *
  * This file contains all configurable settings for the MiroTalk SFU application.
--- public/js/Brand.js
+++ public/js/Brand.js
@@ -80,7 +80,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.0.96</strong>',
+        title: '<strong>WebRTC SFU v2.0.97</strong>',
         html: `
             <button 
                 id="support-button" 
