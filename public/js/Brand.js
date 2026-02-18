f692be1dc4296eb16cd549003a548bfdaae4306a
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-02-18 17:30:26 +0100

    [mirotalksfu] - enable audio in getScreenConstraints

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -82,7 +82,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.1.14</strong>',
+        title: '<strong>WebRTC SFU v2.1.15</strong>',
         html: `
             <button 
                 id="support-button" 
