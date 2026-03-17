33da6be6edeec9e7667e488584d1c132aaa41406
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-03-17 08:26:00 +0100

    [mirotalksfu] - Update .env.template: sync whoAreYou, login, and brand vars with config

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -96,7 +96,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.1.49</strong>',
+        title: '<strong>WebRTC SFU v2.1.50</strong>',
         html: `
             <button 
                 id="support-button" 
