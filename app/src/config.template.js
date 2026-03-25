618def683def8349b9e17f311d9a09b785e7ae3b
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-03-25 11:31:00 +0100

    [mirotalksfu] - fix: override browser autofill yellow background to match dark theme

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk SFU v2.1.62 - Configuration File
+ * MiroTalk SFU v2.1.63 - Configuration File
  * ==============================================
  *
  * This file contains all configurable settings for the MiroTalk SFU application.
--- public/js/Brand.js
+++ public/js/Brand.js
@@ -99,7 +99,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.1.62</strong>',
+        title: '<strong>WebRTC SFU v2.1.63</strong>',
         html: `
             <button 
                 id="support-button" 
