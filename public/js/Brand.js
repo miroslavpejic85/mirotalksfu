04a7faaadefffc8284c36b3e14ff487d3d98fac3
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-03-28 17:43:48 +0100

    [mirotalksfu] - refactor(css): modernize select inputs with theme-aware styling

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -99,7 +99,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.1.65</strong>',
+        title: '<strong>WebRTC SFU v2.1.67</strong>',
         html: `
             <button 
                 id="support-button" 
