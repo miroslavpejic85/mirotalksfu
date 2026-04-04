2ba783a1ffbf6e8770f557097ab80aba64018685
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-04-04 23:28:42 +0200

    [mirotalksfu] - improve dropdown menu and switch row styles: fix invisible hover, reduce padding, add transitions and divider styling

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -99,7 +99,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.1.83</strong>',
+        title: '<strong>WebRTC SFU v2.1.84</strong>',
         html: `
             <button 
                 id="support-button" 
