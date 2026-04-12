7468d18ccfe6f909713f4dc23fad04b6ca4baa68
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-04-12 17:35:59 +0200

    [mirotalksfu] - #247 fix: add null guard in showVideoImageSelector to prevent crash on Edge

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -101,7 +101,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.2.02</strong>',
+        title: '<strong>WebRTC SFU v2.2.03</strong>',
         html: `
             <button 
                 id="support-button" 
