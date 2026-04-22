98cb465fcadf38228f174eff22f004790e75a29f
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-04-22 03:02:33 +0200

    [mirotalksfu] - feat(chat): redesign conversations panel UI

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -101,7 +101,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.2.15</strong>',
+        title: '<strong>WebRTC SFU v2.2.16</strong>',
         html: `
             <button 
                 id="support-button" 
