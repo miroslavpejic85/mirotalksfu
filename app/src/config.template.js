0e0587e1e4c2bab904dd40fb826639711638edfc
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-04-24 14:34:35 +0200

    [mirotalksfu] - add mirotalk linkedin

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk SFU v2.2.18 - Configuration File
+ * MiroTalk SFU v2.2.19 - Configuration File
  * ==============================================
  *
  * This file contains all configurable settings for the MiroTalk SFU application.
