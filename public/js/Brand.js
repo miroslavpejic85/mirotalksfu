bf7dc103cdf3383ae6916e42074edf5495ac620e
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-05-01 16:53:31 +0200

    [mirotalksfu] - improve poll UI styling

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -101,7 +101,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.2.32</strong>',
+        title: '<strong>WebRTC SFU v2.2.33</strong>',
         html: `
             <button 
                 id="support-button" 
