37e534256ac89399f61108d514564d93e4e4c020
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-05-01 16:47:48 +0200

    [mirotalksfu] - refine breakout room panel UI

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk SFU v2.2.31 - Configuration File
+ * MiroTalk SFU v2.2.32 - Configuration File
  * ==============================================
  *
  * This file contains all configurable settings for the MiroTalk SFU application.
--- public/js/Brand.js
+++ public/js/Brand.js
@@ -101,7 +101,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.2.31</strong>',
+        title: '<strong>WebRTC SFU v2.2.32</strong>',
         html: `
             <button 
                 id="support-button" 
