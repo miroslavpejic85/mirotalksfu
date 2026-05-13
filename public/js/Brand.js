ae9438348eb3defd9c805657afcb33babaf7d225
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-05-13 17:51:03 +0200

    [mirotalksfu] - update dep

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -101,7 +101,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.2.54</strong>',
+        title: '<strong>WebRTC SFU v2.2.55</strong>',
         html: `
             <button 
                 id="support-button" 
