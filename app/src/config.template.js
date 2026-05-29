bf013f60c289897a34e403e65cfe24494338d5cd
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-05-29 08:51:58 +0200

    [mirotalksfu] - chore: move TestMuAI from current to past sponsors, update dep

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk SFU v2.2.86 - Configuration File
+ * MiroTalk SFU v2.2.87 - Configuration File
  * ==============================================
  *
  * This file contains all configurable settings for the MiroTalk SFU application.
--- public/js/Brand.js
+++ public/js/Brand.js
@@ -101,7 +101,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.2.86</strong>',
+        title: '<strong>WebRTC SFU v2.2.87</strong>',
         html: `
             <button 
                 id="support-button" 
