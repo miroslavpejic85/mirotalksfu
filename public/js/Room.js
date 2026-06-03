f6c59940c852688f6e43943c09a81193a57ac0db
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-06-03 04:36:08 +0200

    [mirotalksfu] - feat(sound) add test speaker mp3, update dep

--- public/js/Room.js
+++ public/js/Room.js
@@ -11,7 +11,7 @@ if (location.href.substr(0, 5) !== 'https') location.href = 'https' + location.h
  * @license For commercial or closed source, contact us at license.mirotalk@gmail.com or purchase directly via CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-sfu-webrtc-realtime-video-conferences/40769970
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 2.2.90
+ * @version 2.2.91
  *
  */
 
@@ -2404,7 +2404,7 @@ function handleButtons() {
         VideoAI.quality = e.target.value;
     };
     speakerTestBtn.onclick = () => {
-        playSpeaker(speakerSelect?.value, 'ring');
+        playSpeaker(speakerSelect?.value, 'speaker');
     };
     roomId.onclick = () => {
         isMobileDevice ? shareRoom(true) : copyRoomURL();
@@ -5060,7 +5060,7 @@ function setupQuickDeviceSwitchDropdowns() {
         testBtn.appendChild(testIcon);
         testBtn.appendChild(document.createTextNode(' Test Speaker'));
         testBtn.addEventListener('click', () => {
-            playSpeaker(speakerSelect?.value, 'ring');
+            playSpeaker(speakerSelect?.value, 'speaker');
         });
         audioMenu.appendChild(testBtn);
 
@@ -7474,7 +7474,7 @@ function showAbout() {
         position: 'center',
         imageUrl: BRAND.about?.imageUrl && BRAND.about.imageUrl.trim() !== '' ? BRAND.about.imageUrl : image.about,
         customClass: { image: 'img-about' },
-        title: BRAND.about?.title && BRAND.about.title.trim() !== '' ? BRAND.about.title : 'WebRTC SFU v2.2.90',
+        title: BRAND.about?.title && BRAND.about.title.trim() !== '' ? BRAND.about.title : 'WebRTC SFU v2.2.91',
         html: renderRoomTemplate('popupAboutTemplate', {
             html: {
                 aboutContent: BRAND.about.html,
