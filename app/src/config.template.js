66d726e8f38cb9fe34f306a9480dc44384e929f4
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-08-14 12:44:47 +0200

    [mirotalksfu] - update dep

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk SFU v2.3.58 - Configuration File
+ * MiroTalk SFU v2.3.59 - Configuration File
  * ==============================================
  *
  * This file contains all configurable settings for the MiroTalk SFU application.
