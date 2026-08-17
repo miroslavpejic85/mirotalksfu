14fe066ba50c87a0c50e5f3175524fae1f76bb03
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-08-17 09:29:36 +0200

    [mirotalksfu] - style(room): make raised-hand icon a visible circular badge

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -101,7 +101,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.3.66</strong>',
+        title: '<strong>WebRTC SFU v2.3.67</strong>',
         html: `
             <button 
                 id="support-button" 
