bd5749bf06d3aa6939a3089269a8059bcd9bd751
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-08-26 22:25:45 +0200

    [mirotalksfu] feat(widget): add draggable maker option and preserve initial position

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk SFU v2.3.83 - Configuration File
+ * MiroTalk SFU v2.3.84 - Configuration File
  * ==============================================
  *
  * This file contains all configurable settings for the MiroTalk SFU application.
