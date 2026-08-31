43f4a0fdb1883fc8e83be671269ff5cf01520da0
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-08-31 17:30:14 +0200

    [mirotalksfu] - feat(ui): Improve settings UI clarity and responsive layout

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk SFU v2.4.10 - Configuration File
+ * MiroTalk SFU v2.4.11 - Configuration File
  * ==============================================
  *
  * This file contains all configurable settings for the MiroTalk SFU application.
