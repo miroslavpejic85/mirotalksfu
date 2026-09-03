ce4c2b7e87ed5767b54ef3fc1ad5afedd31c9d9d
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-09-03 14:51:45 +0200

    [mirotaksfu] - fix(ui): Improve custom color toggle alignment and translations

--- public/js/Brand.js
+++ public/js/Brand.js
@@ -101,7 +101,7 @@ let BRAND = {
     },
     about: {
         imageUrl: '../images/mirotalk-logo.gif',
-        title: '<strong>WebRTC SFU v2.4.22</strong>',
+        title: '<strong>WebRTC SFU v2.4.23</strong>',
         html: `
             <div class="about-content">
                 <p class="about-description">
