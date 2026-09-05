578ca1c5c980be95e7b7c41e42ebcda77c961288
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-09-05 13:50:19 +0200

    [mirotalksfu] - fix(ui): adapt panel active states to room theme

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk SFU v2.4.39 - Configuration File
+ * MiroTalk SFU v2.4.40 - Configuration File
  * ==============================================
  *
  * This file contains all configurable settings for the MiroTalk SFU application.
