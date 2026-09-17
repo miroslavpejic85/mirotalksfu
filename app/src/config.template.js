518af7d68572184bb436a9732fcf347b0183be02
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-09-18 00:06:50 +0200

    [mirotalksfu] - feat(landing): link commercial license support directly

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -2,7 +2,7 @@
 
 /**
  * ==============================================
- * MiroTalk SFU v2.4.63 - Configuration File
+ * MiroTalk SFU v2.4.64 - Configuration File
  * ==============================================
  *
  * This file contains all configurable settings for the MiroTalk SFU application.
